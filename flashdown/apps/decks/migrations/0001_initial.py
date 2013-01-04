cf2a44f0d710c3c532be70014484fe25e629592a
Justin McManus <jmcmanu@gmail.com>
2013-01-04 15:51:17 -0800

    Initial commit of new schema.

--- flashdown/apps/decks/migrations/0001_initial.py
+++ flashdown/apps/decks/migrations/0001_initial.py
@@ -12,62 +12,139 @@ class Migration(SchemaMigration):
         db.create_table('decks_tag', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('is_deck', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('deleted', self.gf('django.db.models.fields.BooleanField')(default=False)),
         ))
         db.send_create_signal('decks', ['Tag'])
+
+        # Adding model 'Deck'
+        db.create_table('decks_deck', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('description', self.gf('django.db.models.fields.TextField')()),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('owner', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('private', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('active', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('diff_from_parent', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('parent', self.gf('mptt.fields.TreeForeignKey')(blank=True, related_name='forks', null=True, to=orm['decks.Deck'])),
+            ('super_deck', self.gf('mptt.fields.TreeForeignKey')(blank=True, related_name='sub_decks', null=True, to=orm['decks.Deck'])),
+            ('top_level', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('lft', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
+            ('rght', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
+            ('tree_id', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
+            ('level', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
+        ))
+        db.send_create_signal('decks', ['Deck'])
+
+        # Adding M2M table for field tags on 'Deck'
+        db.create_table('decks_deck_tags', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('deck', models.ForeignKey(orm['decks.deck'], null=False)),
+            ('tag', models.ForeignKey(orm['decks.tag'], null=False))
+        ))
+        db.create_unique('decks_deck_tags', ['deck_id', 'tag_id'])
 
         # Adding model 'Card'
         db.create_table('decks_card', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('deck', self.gf('django.db.models.fields.related.ForeignKey')(related_name='deck_cards', to=orm['decks.Deck'])),
             ('front', self.gf('django.db.models.fields.TextField')()),
             ('back', self.gf('django.db.models.fields.TextField')()),
-            ('deck', self.gf('django.db.models.fields.related.ForeignKey')(related_name='deck_cards', to=orm['decks.Tag'])),
-            ('deleted', self.gf('django.db.models.fields.BooleanField')(default=False)),
             ('last_asked', self.gf('django.db.models.fields.DateTimeField')(null=True)),
             ('next_due', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
             ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('owner', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('active', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('private', self.gf('django.db.models.fields.BooleanField')(default=False)),
         ))
         db.send_create_signal('decks', ['Card'])
-
-        # Adding M2M table for field tags on 'Card'
-        db.create_table('decks_card_tags', (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('card', models.ForeignKey(orm['decks.card'], null=False)),
-            ('tag', models.ForeignKey(orm['decks.tag'], null=False))
-        ))
-        db.create_unique('decks_card_tags', ['card_id', 'tag_id'])
 
 
     def backwards(self, orm):
         # Deleting model 'Tag'
         db.delete_table('decks_tag')
 
+        # Deleting model 'Deck'
+        db.delete_table('decks_deck')
+
+        # Removing M2M table for field tags on 'Deck'
+        db.delete_table('decks_deck_tags')
+
         # Deleting model 'Card'
         db.delete_table('decks_card')
 
-        # Removing M2M table for field tags on 'Card'
-        db.delete_table('decks_card_tags')
-
 
     models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
         'decks.card': {
             'Meta': {'object_name': 'Card'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'back': ('django.db.models.fields.TextField', [], {}),
             'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'deck': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'deck_cards'", 'to': "orm['decks.Tag']"}),
-            'deleted': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'deck': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'deck_cards'", 'to': "orm['decks.Deck']"}),
             'front': ('django.db.models.fields.TextField', [], {}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'last_asked': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
             'next_due': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'tags': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'tagged_cards'", 'symmetrical': 'False', 'to': "orm['decks.Tag']"})
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'private': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'decks.deck': {
+            'Meta': {'object_name': 'Deck'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'diff_from_parent': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'parent': ('mptt.fields.TreeForeignKey', [], {'blank': 'True', 'related_name': "'forks'", 'null': 'True', 'to': "orm['decks.Deck']"}),
+            'private': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'super_deck': ('mptt.fields.TreeForeignKey', [], {'blank': 'True', 'related_name': "'sub_decks'", 'null': 'True', 'to': "orm['decks.Deck']"}),
+            'tags': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['decks.Tag']", 'symmetrical': 'False'}),
+            'top_level': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
         },
         'decks.tag': {
             'Meta': {'object_name': 'Tag'},
-            'deleted': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_deck': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         }
     }
